e097933f2c170468f531f7710b922f07af8809f9
Sagar Sutar <sagy26.1991@gmail.com>
2017-04-13 01:29:56 +0530

    breadcrump add

--- visualdesign/assets/js/script.js
+++ visualdesign/assets/js/script.js
@@ -2,10 +2,23 @@ $(function(){
     $("#btn-new-inquiry").on('click', function(){
         // $("#inquiry-list-wrapper").toggleClass("hidden");
         // $("#new-inquiry-wrapper").toggleClass("hidden");
-        $(this).parents("#container").find(".wrapper").toggleClass("hidden");
+        $(this).parents("#inquiry").find(".wrapper").toggleClass("hidden");
     });
+    
+    $("#btn-new-appointment").on('click', function(){
+        // $("#inquiry-list-wrapper").toggleClass("hidden");
+        // $("#new-inquiry-wrapper").toggleClass("hidden");
+        $(this).parents("#appointments").find(".wrapper").toggleClass("hidden");
+    });
+    
     
     $(".btn-cancel").on('click', function(){
         $(this).parents("#container").find(".wrapper").toggleClass("hidden");
+    });
+    
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      var target = $(e.target).attr("href") // activated tab
+      // $("#inquiry-list-wrapper").toggleClass("hidden");
+      // $("#new-inquiry-wrapper").toggleClass("hidden");
     });
 });
